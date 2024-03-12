eb2e21e5dfa111bccd9d7ecfe2d28f7c772308c8
E-Mello <e.mello.dev@gmail.com>
2024-03-12 16:20:40 -0400

    Added the answers from the VeryEasyChallenges.md file

---
+++ Aula_10-JS_Exercises/AnswerVeryEasyChallenges.js
@@ -0,0 +1,207 @@
+// ## 1. Retorne a soma de dois números
+
+// function soma(a, b) {
+//   return a + b;
+// }
+
+// console.log(soma(4, 4));
+
+// ## 2. Converter minutos em segundos
+
+// function convert(min) {
+//   return min * 60;
+// }
+
+// console.log(convert(30));
+
+// ## 3. Retorne o próximo número do número inteiro passado
+
+// function nextInt(params) {
+//   if (typeof params === "number" && Number.isInteger(params)) {
+//     return params + 1;
+//   } else {
+//     console.log("Por gentileza, fornecer um numero inteiro");
+//   }
+// }
+
+// console.log(nextInt(-2));
+
+// ## 4. Área de um Triângulo
+
+// function calcAreaTriangle(base, height) {
+//   return (base * height) / 2;
+// }
+
+// console.log(calcAreaTriangle(2, 3));
+
+// ## 5. Converter idade para dias
+
+// function convertAgeToDays(age) {
+//   return age * 365;
+// }
+
+// console.log(convertAgeToDays(20));
+
+// ## 6. Código bugado (Parte 1)
+
+// function cubes(a) {
+//   return a ** 3;
+// }
+
+// console.log(cubes(3));
+
+// ## 7. Retornar o primeiro elemento em uma matriz
+
+// function getFirstValue(params) {
+//   return params[0];
+// }
+
+// console.log(getFirstValue([1, 2, 3]));
+
+// ## 8. Calculadora de potência
+
+// function circuitPower(volt, elet) {
+//   return volt * elet;
+// }
+
+// console.log(circuitPower(230, 10));
+// console.log(circuitPower(110, 3));
+// console.log(circuitPower(480, 20));
+
+// ## 9. Converter horas em segundos
+
+// function howManySeconds(num) {
+//   return num * 3600;
+// }
+
+// console.log(howManySeconds(2));
+// console.log(howManySeconds(10));
+// console.log(howManySeconds(24));
+
+// ## 10. Borda máxima de um triângulo
+
+// function nextEdge(side1, side2) {
+//   return side1 + side2 - 1;
+// }
+
+// console.log(nextEdge(8, 10));
+// console.log(nextEdge(5, 7));
+// console.log(nextEdge(9, 2));
+
+// ## 11. Retorne o restante de dois números
+
+// function remainder(num01, num02) {
+//   return num01 % num02;
+// }
+
+// console.log(remainder(1, 3));
+// console.log(remainder(3, 4));
+// console.log(remainder(-9, 45));
+// console.log(remainder(5, 5));
+
+// ## 12. Encontre o perímetro de um retângulo
+
+// function findPerimeter(base, height) {
+//   return 2 * (base + height);
+// }
+
+// console.log(findPerimeter(6, 7));
+// console.log(findPerimeter(20, 10));
+// console.log(findPerimeter(2, 9));
+
+// ## 13. Devolva algo para mim
+
+// function giveMeSomething(text) {
+//   return "something " + text;
+// }
+// console.log(giveMeSomething("is better than nothing"));
+// console.log(giveMeSomething("Bob Jane"));
+// console.log(giveMeSomething("something"));
+
+// ## 14. Corrija os erros
+
+// function squared(a) {
+//   return a * a;
+// }
+// console.log(squared(5));
+// console.log(squared(9));
+// console.log(squared(100));
+
+// ## 15. O número é menor ou igual a zero?
+
+// function lessThanOrEqualToZero(num) {
+//   if (num <= 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(lessThanOrEqualToZero(5));
+// console.log(lessThanOrEqualToZero(0));
+// console.log(lessThanOrEqualToZero(-2));
+
+// ## 16. Soma dos ângulos poligonais
+
+// function sumPolygon(n) {
+//   return (n - 2) * 180;
+// }
+
+// console.log(sumPolygon(3));
+// console.log(sumPolygon(4));
+// console.log(sumPolygon(6));
+
+// ## 17. Pontos de basquete
+
+// function points(points1, points2) {
+//   return points1 * 2 + points2 * 3;
+// }
+
+// console.log(points(1, 1));
+// console.log(points(7, 5));
+// console.log(points(38, 8));
+
+// ## 18. Atribuição Básica de Variáveis
+
+// function nameString(str) {
+//   return str + "Edabit";
+// }
+
+// console.log(nameString("Mubashir"));
+// console.log(nameString("Matt"));
+// console.log(nameString("javaScript"));
+
+// ## 19. Menos de 100?
+
+// function lessThan100(num01, num02) {
+//   if (num01 + num02 < 100) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(lessThan100(22, 15));
+// console.log(lessThan100(83, 34));
+// console.log(lessThan100(3, 77));
+
+// ## 20. O problema da fazenda
+
+// function animals(a01, a02, a03) {
+//   return a01 * 2 + a02 * 4 + a03 * 4;
+// }
+
+// console.log(animals(2, 3, 5));
+// console.log(animals(1, 2, 3));
+// console.log(animals(5, 2, 8));
+
+// ## 21. Usando o operador "&&"
+
+// function and(arg01, arg02) {
+//   return arg01 && arg02;
+// }
+
+// console.log(and(true, false));
+// console.log(and(true, true));
+// console.log(and(false, true));
+// console.log(and(false, false));
